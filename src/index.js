bb691aab978970b093c1d539c410448af7bedb4a
tomioka-k <62204434+tomioka-k@users.noreply.github.com>
2021-08-12 21:31:47 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import "./styles.css";
+// var  val1 = "var";
+// console.log(val1)
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数は上書き可能
+
+// val1 = "uwagaki"
+// console.log(val1);
+
+// // var変数の再宣言
+
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2)
+
+// val2 = "let変数を上書き";
+// console.log(val2)
+
+// let val2 = "上書き";
+
+const val3 = "const変数";
+console.log(val3);
